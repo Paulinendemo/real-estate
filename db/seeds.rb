8b873a850f2c8fef857d76037ccba4e00a317b8a
Paulinendemo <pauline.ndemo@student.moringaschool.com>
2022-12-05 11:46:42 +0300

    done seeding data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,84 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "📃 Seeding data..."
+5.times do
+    Buyer.create(
+      username: Faker::Name.name,
+      email: Faker::Internet.email,
+      password_digest: Faker::Internet.password
+    )  
+end
+
+  
+5.times do
+    Seller.create(
+        name: Faker::Name.name,
+        email: Faker::Internet.email,
+        review: Faker::Lorem.sentence
+    )
+end 
+
+
+
+property_one = Property.create(
+    image_url: "https://images.unsplash.com/photo-1568605114967-8130f3a36994?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    name: "Willow Creek Estancia",
+    address: "Marblehead Neck, Marblehead, MA, USA",
+    description: "Willow Creek Estancia will appeal to many different lifestyles, but it has the most perks for equestrians, who will be drawn to its 27-stall main barn, additional eight-stall barn and sand arenas.",
+    price: Faker::Number.number(digits: 6),
+    seller_id: Seller.all.sample.id,
+    buyer_id: Buyer.all.sample.id
+)
+
+property_two = Property.create(
+    image_url: "https://images.unsplash.com/photo-1523217582562-09d0def993a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+    name: "Stonewall Farm",
+    address: "Marblehead Neck, Marblehead, MA, USA",
+    description: "Stonewall Farm, which was featured on our list last year, still hasn’t found a buyer willing to shell out $100 million. The epic, 740-acre equestrian estate is Westchester County’s largest, and has produced the winning horses of 40 stakes races.",
+    price: Faker::Number.number(digits: 6),
+    seller_id: Seller.all.sample.id,
+    buyer_id: Buyer.all.sample.id
+)
+
+property_three = Property.create(
+    image_url: "https://images.unsplash.com/photo-1570129477492-45c003edd2be?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    name: "Pumpkin Key, Florida Keys",
+    address: "Marblehead Neck, Marblehead, MA, USA",
+    description: "Pumpkin Key has been a regular on this list as it’s been taken on and off the market over the years. That being said, there’s no better time to buy a private island than now, when we all wish that we’d had one last year.",
+    price: Faker::Number.number(digits: 6),
+    seller_id: Seller.all.sample.id,
+    buyer_id: Buyer.all.sample.id
+)
+
+property_four = Property.create(
+    image_url: "https://images.unsplash.com/photo-1568605114967-8130f3a36994?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    name: "Willow Creek Estanci",
+    address: "Marblehead Neck, Marblehea, MA, USA",
+    description: "Willow Creek Estancia will appeal to many different lifestyles, but it has the most perks for equestrians, who will be drawn to its 27-stall main barn, additional eight-stall barn and sand arenas.",
+    price: Faker::Number.number(digits: 6),
+    seller_id: Seller.all.sample.id,
+    buyer_id: Buyer.all.sample.id
+)
+
+property_five = Property.create(
+    image_url: "https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=875&q=80",
+    name: "Padaro Lane",
+    address: "Cambridge, MA, USA",
+    description: "Padaro Lane’s biggest perk is its private beach. Situated in the oceanside town of Carpinteria, which is part of Santa Barbara County, it’s a location that already feels relatively quiet and secluded. ",
+    price: Faker::Number.number(digits: 6),
+    seller_id: Seller.all.sample.id,
+    buyer_id: Buyer.all.sample.id
+)
+
+property_six = Property.create(
+    image_url: "https://images.unsplash.com/photo-1583608205776-bfd35f0d9f83?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    name: "Villa in the Hills",
+    address: "Richmond, VA, USA",
+    description: "The Village House is a husband and wife owned restaurant on the Gatineau River in Wakefield, Quebec.",
+    price: Faker::Number.number(digits: 6),
+    seller_id: Seller.all.sample.id,
+    buyer_id: Buyer.all.sample.id
+)
+
+
+
+puts "✅ Done seeding"
+
